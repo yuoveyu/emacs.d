7a55e5a21e5493917b1f5dc5aed9d72a1abe7d6e
boostyu <yuoveyu@163.com>
2022-12-23 20:21:59 +0800

    update 启动界面

--- lisp/init-start.el
+++ lisp/init-start.el
@@ -3,11 +3,21 @@
   :ensure t
   :config
   (setq dashboard-banner-logo-title "Welcome to yumore Emacs!") ;; 个性签名，随读者喜好设置
-  ;; (setq dashboard-projects-backend 'projectile) ;; 读者可以暂时注释掉这一行，等安装了 projectile 后再使用
-  (setq dashboard-startup-banner 'official) ;; 也可以自定义图片
+   (setq dashboard-projects-backend 'projectile)
   (setq dashboard-items '((recents  . 5)   ;; 显示多少个最近文件
 			  (bookmarks . 5)  ;; 显示多少个最近书签
-			  (projects . 10))) ;; 显示多少个最近项目
+               (agenda . 5)
+			  (projects . 5))) ;; 显示多少个最近项目
   (dashboard-setup-startup-hook))
+
+
+(defun dashboard-insert-custom (list-size)
+  (insert "I love emacs day by day!!!"))
+(add-to-list 'dashboard-item-generators  '(custom . dashboard-insert-custom))
+(add-to-list 'dashboard-items '(custom) t)
+(setq dashboard-set-heading-icons t)
+(setq dashboard-set-file-icons t)
+(setq dashboard-set-navigator t)
+(setq dashboard-center-content t)
 
   (provide 'init-start)
