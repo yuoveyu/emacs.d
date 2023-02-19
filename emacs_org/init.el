974d5abf2adbcfd0a1adcc047c5a583e3cf10ea1
boostyu <yuoveyu@163.com>
2023-02-19 21:38:02 +0800

    add spacemacs theme

--- emacs_org/init.el
+++ emacs_org/init.el
@@ -40,7 +40,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package)))
+ '(custom-safe-themes
+   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
+ '(package-selected-packages '(spacemacs-theme use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
